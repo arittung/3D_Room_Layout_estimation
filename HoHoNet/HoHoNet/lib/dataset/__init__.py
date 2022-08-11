b905249add8745427525508f6774f247a3fc1378
Dayoung Kil <53934639+arittung@users.noreply.github.com>
2022-08-11 15:22:26 +0900

    Delete HoHoNet/HoHoNet directory

--- HoHoNet/HoHoNet/lib/dataset/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dataset_layout import PanoCorBonDataset
-from .dataset_s2d3d_sem import S2d3dSemDataset
-from .dataset_depth import CorruptMP3dDepthDataset, MP3dDepthDataset, S2d3dDepthDataset
-
